9d32562ac5b722418de4098fbacebc2ae548149d
NonSpicyBurrito <burritoyangster@gmail.com>
2021-03-10 21:46:24 +0800

    Fixed Spawn using incorrect node func name

--- src/core/scripting/functions/spawn.ts
+++ src/core/scripting/functions/spawn.ts
@@ -2,5 +2,5 @@ import { Code, parse } from '../code'
 import { FuncNode, Node } from '../node'
 
 export function Spawn(id: Code<number>, data: Code[]): Node<0> {
-    return new FuncNode('Play', [parse(id), ...data.map(parse)])
+    return new FuncNode('Spawn', [parse(id), ...data.map(parse)])
 }
