03c99fa318977b3cc3ae7bc2d0cdb2b023cb6451
NonSpicyBurrito <burritoyangster@gmail.com>
2021-08-13 21:30:46 +0800

    Updated entity input for Sonolus 0.5.6

--- src/core/scripting/blocks/entityInput.ts
+++ src/core/scripting/blocks/entityInput.ts
@@ -3,5 +3,6 @@ import { Pointer } from '../pointer'
 export const EntityInput = new Pointer(23)
 
 export const InputJudgment = EntityInput.to<number>(0)
-export const InputBucket = EntityInput.to<number>(1)
-export const InputBucketValue = EntityInput.to<number>(2)
+export const InputAccuracy = EntityInput.to<number>(1)
+export const InputBucket = EntityInput.to<number>(2)
+export const InputBucketValue = EntityInput.to<number>(3)
