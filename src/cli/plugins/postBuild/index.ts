1db7cdc6e4bc7e20d22c988df8bad766d26b06b0
NonSpicyBurrito <burritoyangster@gmail.com>
2023-07-22 03:15:07 +0800

    Added tutorial post build plugin

--- src/cli/plugins/postBuild/index.ts
+++ src/cli/plugins/postBuild/index.ts
@@ -1,5 +1,13 @@
 import { Plugin } from 'esbuild'
 import { FullSonolusCLIConfig } from '../../config.js'
 import { playPostBuildPlugin } from './play/index.js'
+import { tutorialPostBuildPlugin } from './tutorial/index.js'
 
-export const postBuildPlugin = (config: FullSonolusCLIConfig): Plugin => playPostBuildPlugin(config)
+export const postBuildPlugin = (config: FullSonolusCLIConfig): Plugin => {
+    switch (config.type) {
+        case 'play':
+            return playPostBuildPlugin(config)
+        case 'tutorial':
+            return tutorialPostBuildPlugin(config)
+    }
+}
