e18a3164e35da9f1210c65bffaa6a9ee219d8b1b
NonSpicyBurrito <burritoyangster@gmail.com>
2023-07-21 22:39:47 +0800

    Refactored CLI argument parsing

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,11 +3,29 @@
 import { buildCLI } from './build/index.js'
 import { devCLI } from './dev/index.js'
 
-if (process.argv.includes('--dev')) {
-    devCLI()
-} else if (process.argv.includes('--build')) {
-    buildCLI()
-} else {
+const parseArgs = (args: string[]) => {
+    const [mode] = args.slice(args.length - 1)
+    if (mode !== '--dev' && mode !== '--build') return
+
+    return { mode } as const
+}
+
+const parsed = parseArgs(process.argv)
+if (!parsed) {
     console.error('Unknown arguments.')
-    console.error('Supported arguments: --dev, --build')
+    console.log('Usage: sonolus-cli mode')
+    console.log('    mode:')
+    console.log('        --dev       Start development server')
+    console.log('        --build     Build for production')
+
+    process.exit(1)
+}
+
+switch (parsed.mode) {
+    case '--dev':
+        devCLI()
+        break
+    case '--build':
+        buildCLI()
+        break
 }
