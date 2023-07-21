623defafb50679e368e0787f0204823a06721048
NonSpicyBurrito <burritoyangster@gmail.com>
2023-07-22 03:14:05 +0800

    Refactored to split up post build

--- src/cli/plugins/postBuild/index.ts
+++ src/cli/plugins/postBuild/index.ts
@@ -1,21 +1,5 @@
 import { Plugin } from 'esbuild'
-import fs from 'fs-extra'
-import path from 'node:path'
-import { hash } from 'sonolus-core'
 import { FullSonolusCLIConfig } from '../../config.js'
-import { createPlugin } from '../utils.js'
+import { playPostBuildPlugin } from './play/index.js'
 
-const filenames = ['EngineConfiguration', 'EnginePlayData', 'LevelData'] as const
-
-export const postBuildPlugin = (config: FullSonolusCLIConfig): Plugin =>
-    createPlugin('PostBuild', async () => {
-        const buffers = await Promise.all(
-            filenames.map((filename) => fs.readFile(path.join(config.dev, filename))),
-        )
-        const files = buffers.map((buffer, index) => [filenames[index], hash(buffer)])
-
-        console.log()
-        for (const [filename, hash] of files) {
-            console.log(hash, filename)
-        }
-    })
+export const postBuildPlugin = (config: FullSonolusCLIConfig): Plugin => playPostBuildPlugin(config)
