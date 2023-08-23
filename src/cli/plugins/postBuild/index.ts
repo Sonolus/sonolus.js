0eac1f819b0c06631c48413b35347374161a864b
NonSpicyBurrito <burritoyangster@gmail.com>
2023-08-23 11:47:33 +0800

    Added preview post build plugin

--- src/cli/plugins/postBuild/index.ts
+++ src/cli/plugins/postBuild/index.ts
@@ -1,12 +1,15 @@
 import { Plugin } from 'esbuild'
 import { FullSonolusCLIConfig } from '../../config.js'
 import { playPostBuildPlugin } from './play/index.js'
+import { previewPostBuildPlugin } from './preview/index.js'
 import { tutorialPostBuildPlugin } from './tutorial/index.js'
 
 export const postBuildPlugin = (config: FullSonolusCLIConfig): Plugin => {
     switch (config.type) {
         case 'play':
             return playPostBuildPlugin(config)
+        case 'preview':
+            return previewPostBuildPlugin(config)
         case 'tutorial':
             return tutorialPostBuildPlugin(config)
     }
